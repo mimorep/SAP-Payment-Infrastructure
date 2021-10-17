371e32b8a1059e49a16f8009b9dc732dbb48524a
mimorep <mimorep@unirioja.es>
2021-10-17 13:39:19 +0200

    Boton para subir arriba añadido

---
+++ js/up.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * 
+ */
+
+$(document).ready(() => {
+    
+    $('.go-up ').click(() => {
+        $('body, html').animate({
+            scrollTop: '0px'
+        }, 300);
+    });
+
+    $(window).scroll(() => {
+        if ($(this).scrollTop() > 0) {
+            $('.go-up').slideDown(500);
+        } else {
+            $('.go-up').slideUp();
+        }
+    });
+
+});
